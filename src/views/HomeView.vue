c456618ceb0148b1fb656bcdc1479f23bfae610c
cuauhizo <cuauhizo@gmail.com>
2026-07-31 13:12:57 -0600

    ajustes para prod

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,12 +7,12 @@
   useHead({
     title: 'Proxper | Seguridad financiera',
     meta: [
-      { name: 'description', content: 'loreNon qui veniam eu deserunt enim.' },
-      { name: 'keywords', content: 'Mollit consequat quis aliquip pariatur ullamco ullamco.' },
+      { name: 'description', content: 'Descubre Proxper Asesores, expertos en seguridad financiera en México. Protege el patrimonio de tu familia con seguros de vida, gastos médicos y ahorro.' },
+      { name: 'keywords', content: 'Proxper, Asesores financieros en CDMX, Seguros Monterrey en Ciudad de México, Seguridad financiera México, Asesores financieros CDMX, Promotoría de seguros, Seguros de vida, Seguros de gastos médicos' },
 
       // --- FACEBOOK, LINKEDIN, WHATSAPP (Open Graph) ---
       { property: 'og:title', content: 'Proxper | Seguridad financiera' },
-      { property: 'og:description', content: 'Nisi et anim culpa Lorem.' },
+      { property: 'og:description', content: 'Descubre Proxper Asesores, expertos en seguridad financiera en México. Protege el patrimonio de tu familia con seguros de vida, gastos médicos y ahorro.' },
       { property: 'og:type', content: 'website' },
       { property: 'og:url', content: 'https://proxper.com.mx/' },
       { property: 'og:image', content: 'https://proxper.com.mx/proxper.png' },
@@ -21,12 +21,12 @@
       // Definir las dimensiones le dice a las redes el tamaño exacto antes de descargarla (Evita el lag en WhatsApp)
       { property: 'og:image:width', content: '1200' },
       { property: 'og:image:height', content: '630' },
-      { property: 'og:image:alt', content: 'Lorem impsum' },
+      { property: 'og:image:alt', content: 'Proxper | Seguridad financiera' },
 
       // Twitter Card
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:title', content: 'Proxper | Seguridad financiera' },
-      { name: 'twitter:description', content: 'Qui culpa ex aute consectetur eiusmod incididunt reprehenderit nisi velit.' },
+      { name: 'twitter:description', content: 'Descubre Proxper Asesores, expertos en seguridad financiera en México. Protege el patrimonio de tu familia con seguros de vida, gastos médicos y ahorro.' },
       { name: 'twitter:image', content: 'https://proxper.com.mx/proxper.png' },
       { name: 'twitter:url', content: 'https://proxper.com.mx/' },
     ],
